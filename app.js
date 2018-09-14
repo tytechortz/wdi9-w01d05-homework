22cf47bf7f28f94a5c4e3ef308ffbd638b6f250d
James <tytechortz@gmail.com>
2018-09-14 16:57:12 -0600

    Get even

--- app.js
+++ app.js
@@ -77,9 +77,14 @@ for (let i = 10; i <= 400; i++) {
     console.log(i);
 }
 */
-
+/*
 for (let i = 12; i <= 4000; i+=3) {
     console.log(i);
+}
+*/
+for (let i = 1; i <= 100; i++) {
+   if (i % 2 === 0)
+   console.log(i + "<-- is an even number"); 
 }
 
 
