736119e0ffa1ed1e7c52e8a71811bda0812616aa
James <tytechortz@gmail.com>
2018-09-14 15:49:12 -0600

    Q and A

---
+++ app.js
@@ -0,0 +1,23 @@
+//I. 
+// A. Q and A
+
+//1. Use const = a value for a variable that won't change and let for ones that will
+//2. let variable = the new value.
+//3. let old variable = new varaible.
+//4. Assigning a varible means giving a variable a value. Declaring a variable is stating that the variable will be used in the code
+//   but does not need a value assigned at that point.  Define
+//5. Pseudocoding is writing out what you want the code to do in plain English as a way to
+//   get a handle on where you want the code to go without getting bogged down in technicalities to start.
+//6. Planning should be about 75-90% of the time spent and typing the code
+//   should be about 10-25%.
+
+
+
+
+
+
+
+
+
+
+
